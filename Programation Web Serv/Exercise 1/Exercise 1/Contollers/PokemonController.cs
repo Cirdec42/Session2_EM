284657b13af26c8e60638769bf6577324312e1e8
cedric <cedricdaigneault2004@gmail.com>
2023-03-06 14:22:13 -0500

    MAJ P.W.S

---
+++ Programation Web Serv/Exercise 1/Exercise 1/Contollers/PokemonController.cs
@@ -0,0 +1,29 @@
+﻿using ExercicePokemon.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Exercise_1.Contollers
+{
+    public class PokemonController : Controller
+    {
+        public IActionResult Index()
+        {
+            var baseDeDonnees = new FausseBaseDeDonnees();
+            var donnees = baseDeDonnees.ObtenirListePokemons();
+            return View(donnees);
+        }
+
+        public IActionResult Consulter(int id)
+        {
+            var baseDeDonnees = new FausseBaseDeDonnees();
+            var donnees = baseDeDonnees.ObtenirListePokemons();
+            foreach (var element in donnees)
+            {
+                if (element.Id == id)
+                {
+                    return View(element);
+                }
+            }
+            return NotFound();
+        }
+    }
+}
