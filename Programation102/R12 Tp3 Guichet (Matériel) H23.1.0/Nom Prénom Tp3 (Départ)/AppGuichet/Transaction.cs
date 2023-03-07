29f4a3875ba7a952bbcf7315851d0f9f95367e97
cedric <cedricdaigneault2004@gmail.com>
2023-03-07 14:50:54 -0500

    MAJ TP3 Prog2

---
+++ Programation102/R12 Tp3 Guichet (Matériel) H23.1.0/Nom Prénom Tp3 (Départ)/AppGuichet/Transaction.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppGuichet
+{
+    public class Transaction
+    {
+        private DateTime m_date;
+        private int m_montant;
+        private string m_numClient;
+        private SorteTransactions m_sorteTransactions;
+
+        public DateTime Date
+        {
+            get { return m_date; }
+        }
+
+        public int Montant
+        {
+            get { return m_montant; }
+        }
+
+        public string NumClient
+        {
+            get { return m_numClient; }
+        }
+
+        public SorteTransactions SorteTransactions
+        {
+            get { return m_sorteTransactions; }
+        }
+
+
+        public override string ToString()
+        {
+            return (int)SorteTransactions + "," + NumClient + "," + Date + "," + Montant;
+        }
+
+        public Transaction(SorteTransactions pSorte, string pNumClient,DateTime pDate, int pMontant)
+        {
+            m_date = pDate;
+            m_montant = pMontant;
+            m_numClient = pNumClient;
+            m_sorteTransactions = pSorte;
+        }
+
+        public Transaction(string pChaineLue)
+        {
+            char delims = ',';
+            String[] tokens = pChaineLue.Split(delims);
+            m_sorteTransactions = (SorteTransactions)int.Parse(tokens[0]);
+            m_numClient = tokens[1];
+            m_date = (DateTime)DateTime.Parse(tokens[2]);
+            m_montant = int.Parse(tokens[3]);
+        }
+    }
+}
